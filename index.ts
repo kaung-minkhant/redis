419e00137e39e4e093a451a2c17ddae5a4136680
kaung min khant <kaungminkhant.official@gmail.como>
2024-06-21 00:29:07 +0630

    runningggg

--- index.ts
+++ index.ts
@@ -1,11 +1,5 @@
 import { RedisClientType, createClient } from "redis";
 
-const client = createClient();
-
-client.on("error", (error) => console.log("Redis Client Error", error));
-
-await client.connect();
-
 const ONE_WEEK_IN_SECONDS = 7 * 86400;
 const VOTE_SCORE = 432;
 const ARTICLES_PER_PAGE = 25;
@@ -24,7 +18,7 @@ async function articleVote(
   const articleId = article.split(":")[-1];
   const addResult = await client.sAdd("voted:" + articleId, user);
   if (!!addResult) {
-    await client.zIncrBy("score:", VOTE_SCORE, user);
+    await client.zIncrBy("score:", VOTE_SCORE, article);
     await client.hIncrBy(article, "votes", 1);
   }
 }
@@ -64,19 +58,81 @@ async function postArticle(
 async function getArticles(
   client: RedisClientType,
   page: number,
-  order: "score:" | "time:" = "score:"
+  order: "score:" | "time:" | string = "score:"
 ) {
-  const start = (page-1) * ARTICLES_PER_PAGE;
+  const start = (page - 1) * ARTICLES_PER_PAGE;
   const end = start + ARTICLES_PER_PAGE - 1;
 
   const ids = await client.zRange(order, start, end, {
-    REV: true 
-  })
-  const articles: {[key: string]: string}[] = []
+    REV: true,
+  });
+  const articles: any[] = [];
+  console.log('ids', ids)
   for (let id of ids) {
-    const articleData = await client.hGetAll(id)
-    articleData['id'] = id
-    articles.push(articleData)
-  } 
+    const articleData = await client.hGetAll(id);
+    articleData["id"] = id;
+    articles.push(articleData);
+  }
   return articles;
 }
+
+async function addRemoveGroups(
+  client: RedisClientType,
+  articleId: number,
+  toAdd: string[] | null = null,
+  toRemove: string[] | null = null
+) {
+  const article = "article:" + articleId;
+  if (toAdd) {
+    for (let group of toAdd) {
+      client.sAdd("group:" + group, article);
+    }
+  }
+  if (toRemove) {
+    for (let group of toRemove) {
+      client.sRem("group:" + group, article);
+    }
+  }
+}
+
+async function getGroupArticles(
+  client: RedisClientType,
+  group: string,
+  page: number,
+  order: "score:" | "time:" = "score:"
+) {
+  const key = order + group;
+  const exists = await client.exists(key);
+  if (!exists) {
+    client.zInterStore(key, ["group" + group, order], {
+      AGGREGATE: "MAX",
+    });
+    client.expire(key, 60);
+  }
+  return getArticles(client, page, key);
+}
+
+async function main() {
+  const client = await createClient()
+    .on("error", (error) => console.log("Redis Client Error", error))
+    .connect();
+
+  const user = "user:123";
+  const user2 = "user:124";
+  await client.set("article:", 0);
+  const postId = await postArticle(
+    client as any,
+    user,
+    "hello",
+    `https://something`
+  );
+  await postArticle(client as any, user, "hiii", `https://somethingmore`);
+  await articleVote(client as any, user, "article:" + postId);
+  await articleVote(client as any, user2, "article:" + postId);
+  const posts = await getArticles(client as any, 1, 'time:');
+  console.log("Posts with top votes", posts);
+
+  await client.disconnect();
+}
+
+main();
